4bed560a6d31dd06a3a67836f4a6295ffe8e8f89
Luciano Marchueta <lucianomarchueta@gmail.com>
2022-08-23 19:06:10 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <nav className='navbar'>
+        <h1>E-Shop</h1>
+        <ul className='list'>
+          <li>HOME</li>
+          <li>Producto</li>
+          <li>Contacto</li>
+        </ul>
+        <button className='btn'>Login</button>
+      </nav>
+    </header>  
   );
 }
 
