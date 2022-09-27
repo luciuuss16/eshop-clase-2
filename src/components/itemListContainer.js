d826595f0cfefe6b03ea45ac0bcf33f0a2bf8b14
Luciano Marchueta <lucianomarchueta@gmail.com>
2022-09-27 17:23:06 -0300

    modification 2 27/9

--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -2,7 +2,7 @@ import ItemList from './ItemList';
 import customFetch from "../utils/customFetch";
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { products } = require('../utils/products');
+import { products } from require('../utils/products');
 
 const ItemListContainer = () => {
     const [datos, setDatos] = useState([]);
